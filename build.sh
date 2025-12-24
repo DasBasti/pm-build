c075a020d0e222d26daf1f6ac825a0a784107861
Bastian Neumann <neumann.bastian@gmail.com>
2025-12-24 11:51:16 +0100

    build nice

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 source ./init-build-env
 
-bitbake brutzelboy brutzelboy-swupdate brutzelboy-swupdate-full
+# nice -n 10: Lower CPU priority (0=normal, 19=lowest)
+# ionice -c 2 -n 7: Best-effort I/O class, priority 7 (0=highest, 7=lowest)
+nice -n 10 ionice -c 2 -n 7 bitbake brutzelboy brutzelboy-swupdate brutzelboy-swupdate-full
