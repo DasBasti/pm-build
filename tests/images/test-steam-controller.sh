6dc629b46ab4bd55ee4967f2716044aa4ce20ade
Bastian Neumann <neumann.bastian@gmail.com>
2026-01-04 16:30:24 +0100

    fix: steam controller udev rules test by checking mounted images

--- tests/images/test-steam-controller.sh
+++ tests/images/test-steam-controller.sh
@@ -3,15 +3,19 @@
 test_steam_controller_udev_rules_deployed() {
     expect "Steam Controller udev rules to be in the rootfs"
 
-    # Find the rootfs tarball
-    rootfs_tar=$(find $DEPLOY_DIR/images/$MACHINE/ -type l -name "*.rootfs.tar" | head -1)
-    [ -z "$rootfs_tar" ] && { log_error "No rootfs tarball found"; fail; }
+    for img in $DEPLOY_DIR/images/$MACHINE/*.rootfs*.ext4 $DEPLOY_DIR/images/$MACHINE/*.ext4; do
+        [ -e "$img" ] || continue
 
-    # Check if the udev rules file exists in the tarball
-    tar -tf "$rootfs_tar" | grep -q "lib/udev/rules.d/70-steam-controller.rules" || {
-        log_error "70-steam-controller.rules not found in rootfs"
-        fail
-    }
+        mountdir=$(mount_rootfs_image "$img") || { log_error "$img: failed to mount"; fail; }
 
+        if [ ! -e "$mountdir/lib/udev/rules.d/70-steam-controller.rules" ]; then
+            umount_rootfs_image "$mountdir"
+            log_error "$img: 70-steam-controller.rules"
+            fail
+        fi
+
+        umount_rootfs_image "$mountdir"
+        found=1
+    done
     pass
 }
